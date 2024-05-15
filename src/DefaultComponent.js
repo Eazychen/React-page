aee1054623feb0424b716cee558d32a699d3ef52
eazy <eazy_chen@icloud.com>
2024-05-15 14:49:58 +0800

    Update footer content and adjust data layout

--- src/DefaultComponent.js
+++ src/DefaultComponent.js
@@ -2,11 +2,7 @@ import React from "react";
 import HomePage from "./HomePage";
 
 const DefaultComponent = () => {
-	const listdata = [
-		{ title: "精選服務",path: "/services"},
-		{ title: "品牌故事", path: "/about" },  
-        { title: "聯繫我們", path: "/contact"}
-	]
+
 	return (
 		<div>
 			<HomePage/>
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { FaInstagram,FaFacebookF,FaTiktok } from "react-icons/fa";
 
 const UlGroup = ({datas,className})=>{
 	const navigate = useNavigate();
-	return (<ul className={className.navUl}>
+	return (<ul className={className.ul}>
 		{datas.map((data)=> (
-			<li className={className.navLiGroup}key={data.id} onClick={()=>navigate(data.path)}>{data.title}</li>
+			<li className={className.liGroup} key={data.id} onClick={()=>navigate(data.path)}>{data.title}</li>
 		))}
 </ul>)
 }
@@ -21,24 +22,32 @@ const HomePage = () => {
 		<div>
 			<div className="nav">
 				<div className="p-4 text-4xl font-bold text-center">Title</div>
-				<UlGroup datas={datas}  className={{navUl:"nav-ul",navLiGroup:"nav-li-group"}}/>
+				<UlGroup datas={datas}  className={{ul:"nav-ul",liGroup:"nav-li-group"}}/>
 			</div>
 			<footer>
 				<div className="footer">
 	        		<div className="pr-2">
-						<h3 className="text-4xl font-bold text-center">Title</h3>
-						<ul>
-							<li></li>
-							<li></li>
-							<li></li>
+						<h3 className="mb-3 text-4xl font-bold">Title</h3>
+						<ul className="flex justify-start">
+							<li className="footer-first-li"><FaInstagram size={20} color="#fff"/></li>
+							<li className="footer-first-li"><FaFacebookF size={20} color="#fff"/></li>
+							<li className="footer-first-li"><FaTiktok size={20} color="#fff"/> </li>
 						</ul>
 					</div>
-					<UlGroup  datas={datas} className={{}}/>
-					<div className="px-2"></div>
-					<div className="pl-2"> 
+					<UlGroup  datas={datas} className={{liGroup:"footer-li"}}/>
+					<div className="px-2">
+						<ul>
+							<li>常見問題</li>
+							<li className="pt-1">服務條款</li>
+							<li className="pt-1">隱私權服務</li>
+						</ul>
+					</div>
+					<div className="flex flex-col pl-2"> 
 						<h4>訂閱最新消息</h4>
-						<input type="text" className=""/>
-						<button type="button">訂閱</button>
+						<div>
+							<input type="email" className="footer-input" placeholder="Email"/>
+						</div>
+						<button type="button" className="btn-dangrous">訂閱</button>
 					</div>
 				</div>
 				<p className="px-6 mx-20 text-center ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, iste.</p>
