e577cd42752ebf03ec0834db825b0b2416555594
eazy <eazy_chen@icloud.com>
2024-06-25 22:51:31 +0800

    modify homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,8 +2,6 @@ import React from "react";
 import Swiper from "./component/Swiper.js";
 import Header from "./component/Header.js";
 import Footer from "./component/Footer.js";
-import FeatureSection from "./component/FeatureSection.js";
-import MagazineLayout from "./component/MagazineLayout.js";
 
 const HomePage = ({ datas, serviceFeatureSectionDatas }) => {
   return (
