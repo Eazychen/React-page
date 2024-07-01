bae3e15bb2b64dc9a46d6826be732644a87e497f
eazy <eazy_chen@icloud.com>
2024-07-01 16:23:54 +0800

    featureSection layout RWD

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,13 +3,22 @@ import Swiper from "./component/Swiper.js";
 import Header from "./component/Header.js";
 import Footer from "./component/Footer.js";
 import MagazineLayout from "./component/MagazineLayout.js";
+import FeatureSection from "./component/FeatureSection.js";
 
-const HomePage = ({ datas }) => {
+const HomePage = ({ datas, serviceFeatureSectionDatas }) => {
   return (
     <div className="flex min-h-screen flex-col">
       <Header datas={datas} />
       <Swiper />
       <MagazineLayout />
+      <FeatureSection
+        serviceFeatureSectionDatas={serviceFeatureSectionDatas}
+        className={{
+          service: "service",
+          servicePic: "service-pic",
+          serviceParagraph: "service-paragraph",
+        }}
+      />
       <Footer datas={datas} />
     </div>
   );
--- src/component/FeatureSection.js
+++ src/component/FeatureSection.js
@@ -3,9 +3,9 @@ import React from "react";
 const FeatureSection = ({ serviceFeatureSectionDatas }) => {
   return (
     <div className="bg-grey-color py-10">
-      <div className="mx-56 px-6">
-        <div className="featuresectiontitle">
-          <h3 className="p-3 text-6xl">我們的服務</h3>
+      <div className="container-layout container-layout-lg content-layout-xl">
+        <div className="feature-section-title">
+          <h3 className="p-3 text-6xl">相關服務</h3>
         </div>
         {serviceFeatureSectionDatas.map((data, index) => {
           return (
@@ -29,7 +29,7 @@ const SectionComponent = ({ data, className }) => {
   return (
     <div className={className.service}>
       <div className={className.servicePic}>
-        <img src={data.imgUrl} alt="" />
+        <img src={data.imgUrl} alt=""/>
       </div>
       <div className={className.serviceParagraph}>
         <h4>{data.title}</h4>
