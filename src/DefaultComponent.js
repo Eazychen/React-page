5418d210342fd9f8015344d6ae845214f9037412
eazy <eazy_chen@icloud.com>
2024-07-02 23:57:46 +0800

    rebulid sidebar

--- src/DefaultComponent.js
+++ src/DefaultComponent.js
@@ -7,39 +7,10 @@ const DefaultComponent = ({ Component }) => {
     { id: 3, title: "智慧居家", path: "/smartAppliances" },
     { id: 4, title: "聯繫我們", path: "/contact" },
   ];
-  const serviceFeatureSectionDatas = [
-    {
-      title: "電器維修",
-      paragraph: `我們提供專業的電器維修服務。無論是電視、冰箱還是洗衣機我們的技術團隊都能快速診斷和修復問題，確保您的電器儘快恢復正常工作。`,
-      imgUrl: "https://picsum.photos/id/208/600/300",
-    },
-    {
-      title: "高效安裝",
-      paragraph: `我們的安裝服務覆蓋所有類型的電器產品。從智慧居家、廚房電器到大小家電確保每個設備都能安全且正確的安裝為您節省時間精力。`,
-      imgUrl: "https://picsum.photos/id/202/600/300",
-    },
-    {
-      title: "保養服務",
-      paragraph: `為了延長電器的使用壽命，我們提供定期維護服務。我們定期檢查您的設備。預防潛在問題的發生確保電器長期穩定運行。`,
-      imgUrl: "https://picsum.photos/id/204/600/300",
-    },
-    {
-      title: "緊急支援",
-      paragraph: `遇到電器故障我們的緊急支援服務隨時待命，無論何時何地我們都會迅速回應。確保在最短時間內為您解決問題讓您無後顧之憂。`,
-      imgUrl: "https://picsum.photos/id/203/600/300",
-    },
-    {
-      title: "誠心推薦",
-      paragraph: `作為專業電器行的我們，致力於為您提供最佳的家電選擇。我們會按照顧客所提出需求推薦優質家電，幫助您打造理想的居家生活。`,
-      imgUrl: "https://picsum.photos/id/203/600/300",
-    },
-  ];
+
   return (
     <div>
-      <Component
-        datas={datas}
-        serviceFeatureSectionDatas={serviceFeatureSectionDatas}
-      />
+      <Component datas={datas} />
     </div>
   );
 };
--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -1,85 +1,75 @@
 import React from "react";
 
+
+const SideBarItem = ({ imgUrl, imgAlt, title, content, reverse }) => {
+  return (
+    <div className={reverse ? "sidebar-reverse" : "sidebar"}>
+      <div className="sidebar-picture">
+        <img
+          src={imgUrl}
+          alt={imgAlt}
+          className="h-auto w-full rounded-lg align-middle"
+        />
+      </div>
+      <div className="sidebar-paragraph">
+        <h3 className="sidebar-paragraph-title">{title}</h3>
+        <p className="sidebar-paragraph-content">{content}</p>
+      </div>
+    </div>
+  );
+};
+
 const SideBar = () => {
+  const sidebarItems = [
+    {
+      imgUrl: "images/life.jpg",
+      imgAlt: "life",
+      title: "未來居住的創新",
+      content: `智慧家庭技術正在改變我們的居住方式，使家庭更智能、更節能。
+                透過智能冰箱和智能烤爐等設備，這些技術不只增加日常便利，同時也幫助家庭有效管理能源消耗，進而降低環境負擔。`,
+      reverse: false,
+    },
+    {
+      imgUrl: "images/space.jpg",
+      imgAlt: "space",
+      title: "打造舒適高效空間",
+      content: `智慧家庭設計專注於融合最新科技來提升居住體驗。
+                自動窗簾、環境音響系統，以及遠程控制的安全設施，均旨在創造一個便捷和舒適的家庭環境。`,
+      reverse: true,
+    },
+    {
+      imgUrl: "images/protect.jpg",
+      imgAlt: "protect",
+      title: "全面保護您的居所",
+      content: `隨著科技進展，智慧家庭安全系統變得更加全面和精密。
+                從進階的運動感應器到全方位的監控攝影機，這些高科技設備確保您的家園時刻安全。`,
+      reverse: false,
+    },
+    {
+      imgUrl: "images/system.jpg",
+      imgAlt: "system",
+      title: "簡化生活的智能系統",
+      content: `智慧家庭控制系統為家庭日常活動帶來前所未有的便利。
+                通過智能手機或語音命令，您可以輕鬆調節溫度、控制照明或是管理娛樂系統，
+                享受科技帶來的輕鬆生活。`,
+      reverse: true,
+    },
+  ];
   return (
     <div className="bg-gray-100">
       <div className="container-layout container-layout-lg container-layout-xl my-20 md:my-24">
-        <div className="sidebar">
-          <div className="sidebar-picture">
-            <img
-              src="images/life.jpg"
-              alt=""
-              className="h-auto w-full rounded-lg align-middle"
+        {sidebarItems.map((item, index) => {
+          return (
+            <SideBarItem
+              key={index}
+              imgUrl={item.imgUrl}
+              imgAlt={item.imgAlt}
+              title={item.title}
+              content={item.content}
+              reverse={item.reverse}
             />
-          </div>
-          <div className="sidebar-paragraph">
-            <h3 className="sidebar-paragraph-title">未來居住的創新</h3>
-            <p className="sidebar-paragraph-content">
-              智慧家庭技術正在改變我們的居住方式， 使家庭更智能、更節能。
-              <br />
-              透過智能冰箱和智能烤爐等設備， 這些技術不只增加日常便利，
-              <br />
-              同時也幫助家庭有效管理能源消耗，進而降低環境負擔。
-            </p>
-          </div>
-        </div>
-        <div className="sidebar-reverse">
-          <div className="sidebar-picture">
-            <img
-              src="images/space.jpg"
-              alt=""
-              className="h-auto w-full rounded-lg align-middle "
-            />
-          </div>
-          <div className="sidebar-paragraph">
-            <h3 className="sidebar-paragraph-title">打造舒適高效空間</h3>
-            <p className="sidebar-paragraph-content">
-              智慧家庭設計專注於融合最新科技來提升居住體驗。
-              <br />
-              自動窗簾、環境音響系統，以及遠程控制的安全設施，
-              <br />
-              均旨在創造一個便捷和舒適的家庭環境。
-            </p>
-          </div>
-        </div>
-        <div className="sidebar">
-          <div className="sidebar-picture">
-            <img
-              src="images/protect.jpg"
-              alt=""
-              className="h-auto w-full rounded-lg align-middle"
-            />
-          </div>
-          <div className="sidebar-paragraph">
-            <h3 className="sidebar-paragraph-title">全面保護您的居所</h3>
-            <p className="sidebar-paragraph-content">
-              隨著科技進展，智慧家庭安全系統變得更加全面和精密。
-              <br />
-              從進階的運動感應器到全方位的監控攝影機，
-              <br />
-              這些高科技設備確保您的家園時刻安全。
-            </p>
-          </div>
-        </div>
-        <div className="sidebar-reverse">
-          <div className="sidebar-picture">
-            <img
-              src="images/system.jpg"
-              alt=""
-              className="h-auto w-full align-middle"
-            />
-          </div>
-          <div className="sidebar-paragraph">
-            <h3 className="sidebar-paragraph-title">簡化生活的智能系統</h3>
-            <p className="sidebar-paragraph-content">
-              智慧家庭控制系統為家庭日常活動帶來前所未有的便利。
-              <br />
-              通過智能手機或語音命令，您可以輕鬆調節溫度、控制照明或是管理娛樂系統，
-              <br />
-              享受科技帶來的輕鬆生活。
-            </p>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
