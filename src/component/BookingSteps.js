78a957563265053597462e71c9961b227fc828e5
eazy <eazy_chen@icloud.com>
2024-07-06 00:29:09 +0800

    add notice ＆ comment out

--- src/component/BookingSteps.js
+++ src/component/BookingSteps.js
@@ -22,10 +22,11 @@ const BookingSteps = () => {
   //使用 useEffect 設置定時器，定時更新 currentStep 以顯示下一個步驟
   useEffect(() => {
     const interval = setInterval(() => {
-      // 每隔2秒將 currentStep 增加1，並使用%確保步驟循環
+      // 每隔2秒將 currentStep 增加1，並使用%取餘數確保步驟循環，確保不會超過陣列長度。
+      // 因為每次到 5 % 5 都會變回 0
       setCurrentStep((prev) => (prev + 1) % steps.length);
     }, 2000);
-    // 清除定時器，
+    // 清除定時器，回傳值是清除副作用的函數
     return () => clearInterval(interval);
   }, []);
 
@@ -47,7 +48,7 @@ const BookingSteps = () => {
             className="item-center mt-5 flex min-h-[50vh] w-full bg-cover bg-center bg-no-repeat opacity-70 md:justify-end xl:min-h-screen"
             style={{ backgroundImage: "url(images/form-background.jpg)" }}
           ></div>
-          <div className="relative my-10 rounded-lg bg-white bg-opacity-90 p-4 shadow-lg md:absolute md:right-[10%] md:top-1/2 md:w-1/3 md:max-w-sm md:-translate-y-1/2 md:transform">
+          <div className="relative my-10 rounded-lg bg-white bg-opacity-90 p-4 shadow-lg md:absolute md:right-[5%] md:top-1/2 md:w-1/3 md:-translate-y-1/2 md:transform">
             <ul className="steps steps-vertical w-full text-xl text-gray-600">
               {steps.map((step, index) => (
                 <motion.li
