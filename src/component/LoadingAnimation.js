551be57403ed23a0418cd7934be769febd6fe651
eazy <eazy_chen@icloud.com>
2024-07-09 23:08:30 +0800

    add loading bg color

--- src/component/LoadingAnimation.js
+++ src/component/LoadingAnimation.js
@@ -4,12 +4,14 @@ import loadingAnimation from "../animations/Animation - 1720522053005.json";
 
 const LoadingAnimation = () => {
   return (
-    <div className="container-layout container-layout-lg container-layout-xl flex h-screen items-center justify-center bg-gray-800">
-      <Lottie
-        animationData={loadingAnimation}
-        loop={true}
-        style={{ width: 200, height: 200 }} // 調整大小
-      />
+    <div className="h-screen-full bg-gray-800">
+      <div className="container-layout container-layout-lg container-layout-xl flex h-screen items-center justify-center ">
+        <Lottie
+          animationData={loadingAnimation}
+          loop={true}
+          style={{ width: 200, height: 200 }} // 調整大小
+        />
+      </div>
     </div>
   );
 };
