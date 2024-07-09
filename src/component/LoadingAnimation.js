8fbb1dfeeed13e773d0355fe6c15fd6dbe9546f0
eazy <eazy_chen@icloud.com>
2024-07-10 00:53:15 +0800

    loading animation width fixed

--- src/component/LoadingAnimation.js
+++ src/component/LoadingAnimation.js
@@ -5,11 +5,11 @@ import loadingAnimation from "../animations/Animation - 1720522053005.json";
 const LoadingAnimation = () => {
   return (
     <div className="h-screen-full bg-gray-800">
-      <div className="container-layout container-layout-lg container-layout-xl flex h-screen items-center justify-center ">
+      <div className="container-layout container-layout-lg container-layout-xl flex h-screen items-center justify-center">
         <Lottie
           animationData={loadingAnimation}
           loop={true}
-          style={{ width: 200, height: 200 }} // 調整大小
+          style={{ width: 400, height: 200 }} // 調整大小
         />
       </div>
     </div>
